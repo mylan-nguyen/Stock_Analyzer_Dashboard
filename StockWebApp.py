b6cf06e0c5fdaadeaf3c318ca0ca63afcffd676d
Mylan Nguyen <mylannguyen482@gmail.com>
2022-09-25 23:19:59 -0400

    Commit new function to find investment amount in stock

--- StockWebApp.py
+++ StockWebApp.py
@@ -116,15 +116,13 @@ my_info = obj.info
 dividend_rate = my_info['dividendRate']
 int_annual_income = int(annual_income)
 num_shares = int_annual_income/dividend_rate
-st.text(int_annual_income)
-st.text(dividend_rate)
-st.text(num_shares)
-st.subheader(int_annual_income + " divide " + dividend_rate )
-#st.subheader("To earn $"+ int_annual_income+ " a year, at a dividend rate of "+ dividend_rate+ ", you must have "+ num_shares+ " shares.\n")
 
-#market_price = my_info['regularMarketPrice']
-#investment = market_price*num_shares
-#st.subheader("This is equivalent to investing "+ investment+ " in "+ company_name+ " today!\n")
+st.subheader("How much do you need to invest to reach your annual income goal?")
+st.text("To earn $" + int_annual_income + " a year, at a dividend rate of " + dividend_rate + ", you must have " + num_shares + " shares.\n")
+
+market_price = my_info['regularMarketPrice']
+investment = market_price*num_shares
+st.text("This is equivalent to investing " + investment + " in " + company_name + " today!\n")
 
 #Display the close price
 st.header(company_name+" Close Price\n")
